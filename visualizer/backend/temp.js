289cdd566859ffbd27f113a679563dd65efaeca5
jhlod cui <eric_cyr@163.com>
2026-01-21 04:33:47 +0800

    feat(evaluator): complete expression evaluation

--- visualizer/backend/temp.js
+++ visualizer/backend/temp.js
@@ -2,4 +2,7 @@ let x = 10;
 let arr = [1, 2, 3]; 
 let obj = { a: 1, b: "hello" }; 
 console.log("de", x, arr, obj);
-// Output: de 10 [1, 2, 3] {a: 1, b: hello}
+for(let i=0;i<10;i++){
+    console.log("de", x, arr, obj);
+}
+// Output: de 10 [1, 2, 3] {a: 1, b: hello}r
